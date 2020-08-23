53eb0a4662c360b6fed38310d481186ad59ffd4c
Johnny Noble <github-a3bd@johnny.net.au>
2020-08-23 17:04:59 +1000

    Add Tailwind, test with demo form

---
+++ postcss.config.js
@@ -0,0 +1,5 @@
+// const tailwindcss = require("tailwindcss");
+module.exports = {
+	// plugins: [tailwindcss("./tailwind.js"), require("autoprefixer")],
+	plugins: [require("tailwindcss"), require("autoprefixer")],
+};
