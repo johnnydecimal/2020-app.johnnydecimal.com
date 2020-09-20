e88166e31514e81cdb5115bb6b018d6cce34ba3d
Johnny Noble <github-a3bd@johnny.net.au>
2020-09-20 13:26:56 +1000

    Rename userbaseState.machine.ts

--- src/machines/userbaseState.machine.ts
+++ src/machines/userbaseState.machine.ts
@@ -1,10 +1,24 @@
-import { Machine } from "xstate";
+import { Machine } from "@xstate/compiled";
+// import { assign } from "xstate";
+// import { Machine } from "xstate";
 import userbase from "userbase-js";
 
 // import JDMachineProcessor2 from "../jdLogic/jdDetector/jdMachineProcessor2";
 import sortUserbaseData from "../jdLogic/helpers/sortUserbaseData";
 
-const databaseStateMachine = Machine({
+interface DatabaseContext {
+	setJdData: any; // TODO: you know.
+}
+
+interface DatabaseEvent {
+	type: "PLACEHOLDER";
+}
+
+const databaseStateMachine = Machine<
+	DatabaseContext,
+	DatabaseEvent,
+	"databaseMachine"
+>({
 	strict: true,
 
 	id: "databaseState",
@@ -14,7 +28,7 @@ const databaseStateMachine = Machine({
 		init: {
 			invoke: {
 				id: "userbaseDatabaseOpen",
-				src: (context) => {
+				src: (context, _event): any => {
 					userbase.openDatabase({
 						databaseName: "test-2020-09-08-14-16",
 						changeHandler: (userbaseData) => {
